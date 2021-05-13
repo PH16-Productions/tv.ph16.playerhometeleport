1d3f3e1f1dea95517b7c0a4442479e4e4d105459
Shmueli Englard <shmueli.yosef@englard.net>
2021-05-13 16:30:30 -0400

    Fixed bug for not using Whitelist and did code refactoring to improve maintainability

--- src/main/java/tv/ph16/playerhometeleport/Plugin.java
+++ src/main/java/tv/ph16/playerhometeleport/Plugin.java
@@ -14,6 +14,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public final class Plugin extends JavaPlugin {
+    private static final String CANNOT_FIND_A_PLAYER_WITH_THAT_NAME = "Cannot find a player with that name";
+    private static final String SET_COMMAND = "set";
+    private static final String LIST_COMMAND = "list";
+    private static final String REMOVE_COMMAND = "remove";
+    private static final String HOME_COMMAND = "home";
 
     @Override
     public void onEnable() {
@@ -45,42 +50,53 @@ public final class Plugin extends JavaPlugin {
     }
 
     @Override
-    public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, String alias, @NotNull String[] args) {
-        if (command.getName().equalsIgnoreCase("home")) {
+    public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, @NotNull String[] args) {
+        if (command.getName().equalsIgnoreCase(HOME_COMMAND)) {
             if (args.length == 1) {
-                ArrayList<String> completes = new ArrayList<>();
-                if ("set".startsWith(args[0].toLowerCase())) {
-                    completes.add("set");
-                } else if ("remove".startsWith(args[0].toLowerCase())) {
-                    completes.add("remove");
-                } else if ("list".startsWith(args[0].toLowerCase())) {
-                    completes.add("list");
-                }
-                for (OfflinePlayer player : getServer().getWhitelistedPlayers()) {
-                    String name = player.getName();
-                    if (getHome(player) != null && name.toLowerCase().startsWith(args[0].toLowerCase())) {
-                        completes.add(name);
-                    }
-                }
-                return completes;
-            } else if (sender.isOp() && args.length == 2 && (args[0].equalsIgnoreCase("set") || args[0].equalsIgnoreCase("remove"))) {
-                ArrayList<String> completes = new ArrayList<>();
-                for (OfflinePlayer player : getServer().getWhitelistedPlayers()) {
-                    String name = player.getName();
-                    if (name.toLowerCase().startsWith(args[1].toLowerCase())) {
-                        completes.add(name);
-                    }
-                }
-                return completes;
+                return onFirstComplete(args);
+            } else if (sender.isOp() && args.length == 2 && (args[0].equalsIgnoreCase(SET_COMMAND) || args[0].equalsIgnoreCase(REMOVE_COMMAND))) {
+                return onSecondComplete(args);
             }
         }
         return super.onTabComplete(sender, command, alias, args);
     }
 
+    @NotNull
+    private ArrayList<String> onSecondComplete(@NotNull String @NotNull [] args) {
+        ArrayList<String> completes = new ArrayList<>();
+        for (OfflinePlayer player : getServer().getOfflinePlayers()) {
+            String playerName = player.getName();
+            if (playerName != null && playerName.toLowerCase().startsWith(args[1].toLowerCase())) {
+                completes.add(playerName);
+            }
+        }
+        return completes;
+    }
+
+    @NotNull
+    private ArrayList<String> onFirstComplete(@NotNull String @NotNull [] args) {
+        ArrayList<String> completes = new ArrayList<>();
+        if (SET_COMMAND.startsWith(args[0].toLowerCase())) {
+            completes.add(SET_COMMAND);
+        } else if (REMOVE_COMMAND.startsWith(args[0].toLowerCase())) {
+            completes.add(REMOVE_COMMAND);
+        } else if (LIST_COMMAND.startsWith(args[0].toLowerCase())) {
+            completes.add(LIST_COMMAND);
+        }
+        for (OfflinePlayer player : getServer().getOfflinePlayers()) {
+            String playerName = player.getName();
+            if (playerName != null && getHome(player) != null && playerName.toLowerCase().startsWith(args[0].toLowerCase())) {
+                completes.add(playerName);
+            }
+        }
+        return completes;
+    }
+
     @Nullable
     private OfflinePlayer getPlayer(@NotNull String name) {
         for (OfflinePlayer player : getServer().getWhitelistedPlayers()) {
-            if (player.getName().equalsIgnoreCase(name)) {
+            String playerName = player.getName();
+            if (playerName != null && playerName.equalsIgnoreCase(name)) {
                 return player;
             }
         }
@@ -88,77 +104,96 @@ public final class Plugin extends JavaPlugin {
     }
 
     @Override
-    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, String label, @NotNull String[] args) {
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (command.getName().equalsIgnoreCase("home")) {
             if (args.length == 0 && sender instanceof Player) {
-                Player sourceTarget = (Player)sender;
-                Location homeLocation = getHome(sourceTarget);
-                if (homeLocation != null) {
-                    sourceTarget.teleport(homeLocation);
-                } else {
-                    sender.sendMessage("You do not have a home set.");
-                }
+                onOwnHomeCommand(sender);
             } else if (args.length == 1) {
-                if (args[0].equalsIgnoreCase("set") && sender instanceof Player) {
-                    Player source = (Player) sender;
-                    setHome(source, source.getLocation());
-                    sender.sendMessage("Home Set");
-                } else if (args[0].equalsIgnoreCase("remove") && sender instanceof Player) {
-                    Player source = (Player) sender;
-                    removeHome(source);
-                    sender.sendMessage("Home Removed");
-                } else if (args[0].equalsIgnoreCase("list")) {
-                    sender.sendMessage("The following players have a home set:");
-                    for (OfflinePlayer player : getServer().getWhitelistedPlayers()) {
-                        if (getHome(player) != null) {
-                            sender.sendMessage("\t" + player.getName());
-                        }
-                    }
-                } else {
-                    OfflinePlayer target = getPlayer(args[0]);
-                    if (target == null) {
-                        sender.sendMessage("Cannot find a player with that name");
-                    } else {
-                        Location homeLocation = getHome(target);
-                        if (homeLocation != null) {
-                            if (sender instanceof Player) {
-                                Player source = (Player)sender;
-                                source.teleport(homeLocation);
-                            } else {
-                                sender.sendMessage("Players home: " + homeLocation.toString());
-                            }
-                        } else {
-                            sender.sendMessage("That player doesn't have a home yet");
-                        }
-                    }
-                }
+                onCommandOne(sender, args);
             } else if (args.length == 2 && sender.isOp() && sender instanceof Player) {
-                Player source = (Player)sender;
-                if (args[0].equalsIgnoreCase("set")) {
-                    OfflinePlayer target = getPlayer(args[1]);
-                    if (target == null) {
-                        sender.sendMessage("Cannot find a player with that name");
-                    } else {
-                        setHome(target, source.getLocation());
-                        sender.sendMessage("Home Set");
-                    }
-                } else if (args[0].equalsIgnoreCase("remove")) {
-                    OfflinePlayer target = getPlayer(args[1]);
-                    if (target == null) {
-                        sender.sendMessage("Cannot find a player with that name");
-                    } else {
-                        removeHome(target);
-                        sender.sendMessage("Home Removed");
-                    }
-                } else {
-                    sender.sendMessage("Unknown sub command");
-                }
+                onOpCommand(sender, args);
             } else {
                 sender.sendMessage("Unknown sub command");
             }
             return true;
+        }
+        return false;
+    }
+
+    private void onCommandOne(@NotNull CommandSender sender, @NotNull String @NotNull [] args) {
+        if (args[0].equalsIgnoreCase(SET_COMMAND) && sender instanceof Player) {
+            Player source = (Player) sender;
+            setHome(source, source.getLocation());
+            sender.sendMessage("Home Set");
+        } else if (args[0].equalsIgnoreCase(REMOVE_COMMAND) && sender instanceof Player) {
+            Player source = (Player) sender;
+            removeHome(source);
+            sender.sendMessage("Home Removed");
+        } else if (args[0].equalsIgnoreCase(LIST_COMMAND)) {
+            onListCommand(sender);
         } else {
-            return false;
+            onOtherHomeCommand(sender, args[0]);
+        }
+    }
+
+    private void onOpCommand(@NotNull CommandSender sender, @NotNull String @NotNull [] args) {
+        Player source = (Player) sender;
+        if (args[0].equalsIgnoreCase(SET_COMMAND)) {
+            OfflinePlayer target = getPlayer(args[1]);
+            if (target == null) {
+                sender.sendMessage(CANNOT_FIND_A_PLAYER_WITH_THAT_NAME);
+            } else {
+                setHome(target, source.getLocation());
+                sender.sendMessage("Home Set");
+            }
+        } else if (args[0].equalsIgnoreCase(REMOVE_COMMAND)) {
+            OfflinePlayer target = getPlayer(args[1]);
+            if (target == null) {
+                sender.sendMessage(CANNOT_FIND_A_PLAYER_WITH_THAT_NAME);
+            } else {
+                removeHome(target);
+                sender.sendMessage("Home Removed");
+            }
+        } else {
+            sender.sendMessage("Unknown sub command");
+        }
+    }
+
+    private void onOtherHomeCommand(@NotNull CommandSender sender, @NotNull String arg) {
+        OfflinePlayer target = getPlayer(arg);
+        if (target == null) {
+            sender.sendMessage(CANNOT_FIND_A_PLAYER_WITH_THAT_NAME);
+        } else {
+            Location homeLocation = getHome(target);
+            if (homeLocation != null) {
+                if (sender instanceof Player) {
+                    Player source = (Player)sender;
+                    source.teleport(homeLocation);
+                } else {
+                    sender.sendMessage("Players home: " + homeLocation);
+                }
+            } else {
+                sender.sendMessage("That player doesn't have a home yet");
+            }
+        }
+    }
+
+    private void onListCommand(@NotNull CommandSender sender) {
+        sender.sendMessage("The following players have a home set:");
+        for (OfflinePlayer player : getServer().getWhitelistedPlayers()) {
+            if (getHome(player) != null) {
+                sender.sendMessage("    " + player.getName());
+            }
+        }
+    }
+
+    private void onOwnHomeCommand(@NotNull CommandSender sender) {
+        Player sourceTarget = (Player) sender;
+        Location homeLocation = getHome(sourceTarget);
+        if (homeLocation != null) {
+            sourceTarget.teleport(homeLocation);
+        } else {
+            sender.sendMessage("You do not have a home set.");
         }
     }
 }
